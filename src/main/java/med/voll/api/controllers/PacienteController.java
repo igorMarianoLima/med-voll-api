d08aabf8fb0b080398e94fe7df3a5e7f4544df97
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-29 17:13:02 -0300

    aula-02-06: Feat - Add (POST) '/pacientes' endpoint

--- src/main/java/med/voll/api/controllers/PacienteController.java
+++ src/main/java/med/voll/api/controllers/PacienteController.java
@@ -1,10 +1,20 @@
 package med.voll.api.controllers;
 
+import med.voll.api.dtos.paciente.CadastroPacienteDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("pacientes")
 public class PacienteController {
 
+    @PostMapping
+    public String cadastrar(
+            @RequestBody CadastroPacienteDTO paciente
+    ) {
+        System.out.println(paciente);
+        return String.format("Olá paciente %s! Seja bem vindo", paciente.nome());
+    }
 }
