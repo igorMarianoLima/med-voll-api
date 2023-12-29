d2f6d8d262013e8989b08736f4aa669e02a5001a
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-29 16:15:41 -0300

    aula-02-03: Feat - Add notations in 'MedicoController'

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -1,4 +1,10 @@
 package med.voll.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
 public class MedicoController {
+
 }
