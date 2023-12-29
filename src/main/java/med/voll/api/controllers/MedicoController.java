ad4329b284dc1d9b38a967ceaed0007cb5946cf6
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-29 16:23:03 -0300

    aula-02-03: Feat - Create 'cadastrar' POST method in 'MedicoController'

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -1,5 +1,7 @@
 package med.voll.api.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @PostMapping
+    public void cadastrar(
+        @RequestBody String json
+    ) {
+        System.out.println(json);
+    }
 }
