92e79d5058a3df91fce9d747cf4113790c2215c8
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-30 12:11:35 -0300

    aula-03-06: Feat - Add constructor using 'CadastroEnderecoDTO' as parameter in 'Endereco' entity

--- src/main/java/med/voll/api/entities/Endereco.java
+++ src/main/java/med/voll/api/entities/Endereco.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.dtos.endereco.CadastroEnderecoDTO;
 
 @Embeddable
 @Getter
@@ -17,4 +18,14 @@ public class Endereco {
     private String complemento;
     private String cidade;
     private String uf;
+
+    public Endereco(CadastroEnderecoDTO endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+    }
 }
