ac7b9eab76dee15dc66cc96ffe4aa0ae1a94620b
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-31 19:57:58 -0300

    atividade-03-14: Feat - Add bean validator annotations in 'CadastroPacienteDTO'

--- src/main/java/med/voll/api/dtos/paciente/CadastroPacienteDTO.java
+++ src/main/java/med/voll/api/dtos/paciente/CadastroPacienteDTO.java
@@ -1,12 +1,29 @@
 package med.voll.api.dtos.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.dtos.endereco.CadastroEnderecoDTO;
 
 public record CadastroPacienteDTO(
+    @NotBlank
     String nome,
+
+    @NotBlank
+    @Email
     String email,
+
+    @NotBlank
     String telefone,
+
+    @NotBlank
+    @Pattern(regexp = "\\d{11}")
     String cpf,
+
+    @NotNull
+    @Valid
     CadastroEnderecoDTO endereco
 ) {
 }
