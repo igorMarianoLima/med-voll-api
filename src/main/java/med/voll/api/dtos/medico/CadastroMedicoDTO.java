2350c457715ff762a542004e8990e9b380a63611
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-31 16:09:16 -0300

    aula-03-11: Feat - Add annotations to validated 'CadastroMedicoDTO' attributes

--- src/main/java/med/voll/api/dtos/medico/CadastroMedicoDTO.java
+++ src/main/java/med/voll/api/dtos/medico/CadastroMedicoDTO.java
@@ -1,13 +1,35 @@
 package med.voll.api.dtos.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.dtos.endereco.CadastroEnderecoDTO;
 import med.voll.api.enums.Especialidade;
+import org.hibernate.validator.constraints.Length;
 
 public record CadastroMedicoDTO(
+        @NotBlank
         String nome,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        @Length(
+            min = 4,
+            max = 6
+        )
         String crm,
+
+        @NotNull
         Especialidade especialidade,
+
+        @NotNull
+        @Valid
         CadastroEnderecoDTO endereco
 ) {
 }
