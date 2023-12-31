1bfc0a6ad96f1713245c9614cef6735d001798ac
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-31 19:58:59 -0300

    atividade-03-14: Feat - Add 'Paciente' entity properties

--- src/main/java/med/voll/api/entities/Paciente.java
+++ src/main/java/med/voll/api/entities/Paciente.java
@@ -1,4 +1,37 @@
 package med.voll.api.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.dtos.paciente.CadastroPacienteDTO;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(CadastroPacienteDTO paciente) {
+        this.nome = paciente.nome();
+        this.email = paciente.email();
+        this.telefone = paciente.telefone();
+        this.cpf = paciente.cpf();
+        this.endereco = new Endereco(paciente.endereco());
+    }
 }
