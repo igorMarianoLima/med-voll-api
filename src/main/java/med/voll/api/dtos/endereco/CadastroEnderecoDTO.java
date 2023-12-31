e345905995d0708ff055872569d47e1b555b66d8
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-31 16:09:07 -0300

    aula-03-11: Feat - Add annotations to validated 'CadastroEnderecoDTO' attributes

--- src/main/java/med/voll/api/dtos/endereco/CadastroEnderecoDTO.java
+++ src/main/java/med/voll/api/dtos/endereco/CadastroEnderecoDTO.java
@@ -1,11 +1,25 @@
 package med.voll.api.dtos.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record CadastroEnderecoDTO(
+    @NotBlank
     String logradouro,
+
+    @NotBlank
     String bairro,
+
+    @NotBlank
+    @Pattern(regexp = "\\d{8}")
     String cep,
+
+    @NotBlank
     String cidade,
+
+    @NotBlank
     String uf,
+
     String complemento,
     String numero
 ) {
