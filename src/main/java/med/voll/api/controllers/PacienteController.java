ee5c79dbeb55ad3a70e57eb9bc68d05d30919713
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2024-01-01 14:23:10 -0300

    atividade-04-08: Feat - Implements (GET) '/pacientes' endpoint with pagination

--- src/main/java/med/voll/api/controllers/PacienteController.java
+++ src/main/java/med/voll/api/controllers/PacienteController.java
@@ -2,15 +2,15 @@ package med.voll.api.controllers;
 
 import jakarta.validation.Valid;
 import med.voll.api.dtos.paciente.CadastroPacienteDTO;
+import med.voll.api.dtos.paciente.ListarPacienteDTO;
 import med.voll.api.entities.Paciente;
 import med.voll.api.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -26,5 +26,16 @@ public class PacienteController {
     ) {
         Paciente pacienteEntity = this._pacienteService.cadastrar(paciente);
         return new ResponseEntity<>(pacienteEntity, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ListarPacienteDTO>> listarTodos(
+        Pageable pagination
+    ) {
+        Page<ListarPacienteDTO> pacientes = this._pacienteService.listarTodos(pagination).map(
+            paciente -> new ListarPacienteDTO(paciente.getNome(), paciente.getEmail(), paciente.getCpf())
+        );
+
+        return new ResponseEntity<>(pacientes, HttpStatus.OK);
     }
 }
