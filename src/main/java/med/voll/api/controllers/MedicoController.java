fd562674926f57bc22e36ca44acc76425b3b8332
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2023-12-31 23:00:16 -0300

    aula-04-01: Feat - Implement (GET) '/medicos' endpoint

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -4,15 +4,15 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dtos.medico.CadastroMedicoDTO;
 
+import med.voll.api.dtos.medico.ListarMedicoDTO;
 import med.voll.api.entities.Medico;
 import med.voll.api.services.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -30,5 +30,12 @@ public class MedicoController {
         Medico medicoEntity = this._medicoService.cadastrar(medico);
 
         return new ResponseEntity<Medico>(medicoEntity, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListarMedicoDTO>> listarTodos() {
+        List<ListarMedicoDTO> listaMedicos = this._medicoService.listarTodos().stream().map(ListarMedicoDTO::new).toList();
+
+        return new ResponseEntity<>(listaMedicos, HttpStatus.OK);
     }
 }
