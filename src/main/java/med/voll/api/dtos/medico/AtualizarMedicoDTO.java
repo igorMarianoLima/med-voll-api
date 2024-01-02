6de6b07c7abb98e5aff1e3fa08709c96b2e7328b
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2024-01-02 15:21:16 -0300

    aula-05-02: Feat - Create 'AtualizarMedicoDTO' attributes

--- src/main/java/med/voll/api/dtos/medico/AtualizarMedicoDTO.java
+++ src/main/java/med/voll/api/dtos/medico/AtualizarMedicoDTO.java
@@ -1,4 +1,13 @@
 package med.voll.api.dtos.medico;
 
-public record AtualizarMedicoDTO() {
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.dtos.endereco.CadastroEnderecoDTO;
+
+public record AtualizarMedicoDTO(
+    @NotNull
+    Long id,
+    String nome,
+    String telefone,
+    CadastroEnderecoDTO endereco
+) {
 }
