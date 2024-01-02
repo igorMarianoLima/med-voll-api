a921a81f5d205c97c62f40413a8e6d513cf89eff
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2024-01-02 12:00:18 -0300

    aula-05-01: Feat - Add 'id' field in 'ListarMedicoDTO'

--- src/main/java/med/voll/api/dtos/medico/ListarMedicoDTO.java
+++ src/main/java/med/voll/api/dtos/medico/ListarMedicoDTO.java
@@ -4,12 +4,13 @@ import med.voll.api.entities.Medico;
 import med.voll.api.enums.Especialidade;
 
 public record ListarMedicoDTO(
+    Long id,
     String nome,
     String email,
     String crm,
     Especialidade especialidade
 ) {
     public ListarMedicoDTO(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
